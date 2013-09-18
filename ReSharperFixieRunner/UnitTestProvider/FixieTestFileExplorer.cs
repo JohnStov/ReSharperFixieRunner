b38552f80b1d11b629511619016b935a5b7a93ac
John Stovin <john.d.stovin@gmail.co.uk>
2013-09-18 12:36:01 +0100

    Check for Fixie library reference

--- ReSharperFixieRunner/UnitTestProvider/FixieTestFileExplorer.cs
+++ ReSharperFixieRunner/UnitTestProvider/FixieTestFileExplorer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 
 using JetBrains.Application;
+using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Tree;
 using JetBrains.ReSharper.UnitTestFramework;
@@ -22,6 +24,14 @@ namespace ReSharperFixieRunner.UnitTestProvider
         public void ExploreFile(IFile psiFile, UnitTestElementLocationConsumer consumer, CheckForInterrupt interrupted)
         {
             if (interrupted())
+                return;
+
+            // don't bother going any further if there's isn't a project with a reference to the Fixie assembly
+            var project = psiFile.GetProject();
+            if (project == null)
+                return;
+
+            if(project.GetModuleReferences().All(module => module.Name != "Fixie"))
                 return;
 
             psiFile.ProcessDescendants(new FixiePsiFileExplorer(unitTestElementFactory, consumer, psiFile, interrupted));
