dfa93f415c492da51753f1d9fc2f0dd8209bd457
John Stovin <john.d.stovin@gmail.com>
2013-11-13 16:06:43 +0000

    More Unit Tesr Runner work

---
+++ ReSharperFixieRunner/TestRunner/FixieTaskRunner.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.ReSharper.TaskRunnerFramework;
+
+namespace ReSharperFixieRunner.TestRunner
+{
+    class FixieTaskRunner : RecursiveRemoteTaskRunner
+    {
+        public const string RunnerId = "Fixie";
+
+        private IRemoteTaskServer taskServer;
+
+        public FixieTaskRunner(IRemoteTaskServer server)
+            : base(server)
+        {
+            taskServer = server;
+        }
+
+        public override void ExecuteRecursive(TaskExecutionNode node)
+        {
+        }
+    }
+}
