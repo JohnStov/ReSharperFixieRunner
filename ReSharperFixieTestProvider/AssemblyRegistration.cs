432a43307a35191df5adafc59a3a582c7ec3c2dc
John Stovin <john.d.stovin@gmail.com>
2013-12-09 09:50:44 +0000

    AssemblyRegistration class

---
+++ ReSharperFixieTestProvider/AssemblyRegistration.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.ProjectModel;
+using JetBrains.ReSharper.UnitTestFramework;
+
+using ReSharperFixieTestRunner;
+
+namespace ReSharperFixieTestProvider
+{
+    [SolutionComponent]
+    public class AssemblyRegistration
+    {
+        public AssemblyRegistration(UnitTestingAssemblyLoader assemblyLoader)
+        {
+            // Register assemblies needed later by test runner.
+            assemblyLoader.RegisterAssembly(typeof(TaskRunner).Assembly);
+        }
+    }
+}
--- ReSharperFixieTestProvider/FixieTestMetadataExplorer.cs
+++ ReSharperFixieTestProvider/FixieTestMetadataExplorer.cs
@@ -28,10 +28,6 @@ namespace ReSharperFixieTestProvider
             this.provider = provider;
             this.conventionCheck = conventionCheck;
             this.unitTestElementFactory = unitTestElementFactory;
-
-            // Hmm. Not sure I like this here - needs to be here so that ReSharper will load
-            // the runner assembly from the external process, so that assumes this was done
-            assemblyLoader.RegisterAssembly(typeof(TaskRunner).Assembly);
         }
 
 
