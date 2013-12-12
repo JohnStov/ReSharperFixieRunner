02d8bbdac83fda296382f42fa975953f9cfa8fca
John Stovin <john.d.stovin@gmail.com>
2013-12-12 13:19:18 +0000

    Rename project folder to match projects

--- FixiePlugin/Properties/AssemblyInfo.cs
+++ FixiePlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using JetBrains.Application.PluginSupport;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReSharperFixieTestProvider")]
+[assembly: AssemblyTitle("FixiePlugin")]
 [assembly: AssemblyDescription("A Unit Test plugin for the Fixie test framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("John Stovin")]
-[assembly: AssemblyProduct("ReSharperFixieTestProvider")]
+[assembly: AssemblyProduct("FixiePlugin")]
 [assembly: AssemblyCopyright("Copyright © John Stovin, 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,9 +17,9 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
 
-[assembly: ActionsXml("ReSharperFixieRunner.Actions.xml")]
+[assembly: ActionsXml("FixiePlugin.Actions.xml")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
-[assembly: PluginTitle("ReSharperFixieRunner")]
+[assembly: PluginTitle("FixiePlugin")]
 [assembly: PluginDescription("A Unit Test plugin for the Fixie test framework")]
 [assembly: PluginVendor("John Stovin")]
